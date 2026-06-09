7e47efa5e33aa06543de928192067f54fd206562
letsmive-a11y <letsmive@gmail.com>
2026-06-09 07:37:37 +0700

    Update config database Railway

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 # config.py
+import os
 import psycopg2
 
 def connect_db():
+    database_url = os.environ.get("DATABASE_URL")
+
+    # Kalau di Railway, pakai DATABASE_URL
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    # Kalau di laptop, pakai database lokal
     return psycopg2.connect(
         host="localhost",
-        database="spk_game",    
+        database="spk_game",
         user="postgres",
         password="admin",
         port=5432,
